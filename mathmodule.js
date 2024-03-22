1c31e110e30619092a4108f8e015fafb1fd90b5a
TrungPhan-ADVN <trungphan@advn.vn>
2024-03-22 15:26:23 +0700

    Introduction Node JS

---
+++ mathmodule.js
@@ -0,0 +1,11 @@
+ Pi = Math.PI;
+ test_cons = 1;
+// console.log(Pi)
+exports.dientich = function(r){
+    return Pi * r * r
+}
+test_cons +=1
+console.log(test_cons)
+exports.chuvi = function(r){
+    return Pi * r * 2
+}
---
+++ test.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var time = require('./myfirstmodule.js');
+var events = require('events');
+var math = require('./mathmodule.js')
+// var eventEmitter = new events.EventEmitter();
+// var myEventHandler = function(){
+//   console.log('I hear a scream!');
+// }
+
+// eventEmitter.on('scream',myEventHandler);
+// eventEmitter.emit('scream');
+test_cons +=1
+console.log(test_cons)
+console.log(Pi);
+console.log(time.myDateTime())
+console.log(math.dientich(1))
+console.log(math.chuvi(5))
